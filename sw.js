5067f5e08552dda2315af7e69667a5d49b6fcb7a
Scott Wu <scottwu@live.com>
2016-03-01 12:26:57 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@ this.addEventListener('install', function(event) {
     })
   );
 });
-//test4
+//test5
 this.addEventListener('fetch', function(event) {
   var response;
   event.respondWith(caches.match(event.request).catch(function() {
