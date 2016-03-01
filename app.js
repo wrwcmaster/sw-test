3a27b934490237397b5210f9b7f9a4ffa0c01358
Scott Wu <scottwu@scottwu.sha.corp.google.com>
2016-03-01 17:33:55 +0800

    only a test

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
-// register service worker
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' }).then(function(reg) {
-    
-    if(reg.installing) {
-      console.log('Service worker installing');
-    } else if(reg.waiting) {
-      console.log('Service worker installed');
-    } else if(reg.active) {
-      console.log('Service worker active');
-    }
-    
-  }).catch(function(error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
-  });
-};
 
 // function for loading each image via XHR
 
